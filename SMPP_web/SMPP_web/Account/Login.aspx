cdff5c167eb28d1ce18527e9826857c9b56d6ed3
Anthony Latty <marclatty1@gmail.com>
2019-03-25 12:29:52 -0500

    remove unused code and reorganize codebase

--- SMPP_web/SMPP_web/Account/Login.aspx
+++ SMPP_web/SMPP_web/Account/Login.aspx
@@ -1,5 +1,6 @@
 ﻿<%@ Page Title="Log in" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SMPP_web.Account.Login" Async="true" %>
 
+<%@ Register Src="~/Account/OpenAuthProviders.ascx" TagPrefix="uc" TagName="OpenAuthProviders" %>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
     <h2><%: Title %>.</h2>
--- SMPP_web/SMPP_web/Account/Login.aspx.cs
+++ SMPP_web/SMPP_web/Account/Login.aspx.cs
@@ -15,6 +15,7 @@ namespace SMPP_web.Account
             RegisterHyperLink.NavigateUrl = "Register";
             // Enable this once you have account confirmation enabled for password reset functionality
             //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
+            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
             var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
             if (!String.IsNullOrEmpty(returnUrl))
             {
