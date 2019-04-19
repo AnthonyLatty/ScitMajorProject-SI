a9107d40cf98da8da0d4e5c7fc92636fd7e3b304
perplexedyawdie <sunkwenga@gmail.com>
2019-04-19 00:52:42 -0500

    Added Librarian dashboard features

---
+++ SMPP_web/SMPP_web/Account/Librarian/LibrarianDashboard.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="LibrarianDashboard.aspx.cs" Inherits="SMPP_web.Account.Librarian.LibrarianDashboard" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="BannerContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="form-horizontal" style="margin-top: 5%;">
+        <h4>Upload a project</h4>
+        <hr />
+        <div class="row " style="margin-bottom: 2%;">
+            <div class="form-group">
+                <asp:Label runat="server" AssociatedControlID="TitleTxtbx" CssClass="col-md-2 control-label">Title </asp:Label>
+                <div class="col-md-7">
+                    <asp:TextBox runat="server" ID="TitleTxtbx" CssClass="form-control" />
+                    <asp:RequiredFieldValidator ValidationGroup="Lib" ID="RequiredFieldValidator1" runat="server" ControlToValidate="TitleTxtbx"
+                        CssClass="text-danger" ErrorMessage="The title field is required." />
+                </div>
+            </div>
+        </div>
+        <div class="row " style="margin-bottom: 2%;">
+
+            <div class="form-group">
+                <asp:Label runat="server" AssociatedControlID="DateTxtbx" CssClass="col-md-2 control-label">Date </asp:Label>
+                <div class="col-md-7">
+                    <asp:TextBox runat="server" ID="DateTxtbx" CssClass="form-control" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="row" style="margin-bottom: 2%;margin-left:9em;">
+        <div class="col-md-5">
+            <asp:FileUpload ID="ProjectFileUpload" runat="server" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-5" style="margin-left:10em;">
+            <asp:Button ID="UploadBtn" runat="server" Text="Upload" CssClass="btn btn-success" OnClick="UploadBtn_Click" />
+        </div>
+    </div>
+
+</asp:Content>
---
+++ SMPP_web/SMPP_web/Account/Librarian/LibrarianDashboard.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SMPP_web.Models;
+
+namespace SMPP_web.Account.Librarian
+{
+    public partial class LibrarianDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            DateTxtbx.Text = DateTime.Today.ToString().Substring(0, 10);
+
+        }
+
+        protected void UploadBtn_Click(object sender, EventArgs e)
+        {
+            if (ProjectFileUpload.HasFile)
+            {
+                ProjectFileUpload.PostedFile.SaveAs(Request.PhysicalApplicationPath + "Uploads\\" + TitleTxtbx.Text + Path.GetExtension(ProjectFileUpload.PostedFile.FileName));
+                Document uploadingDocument = new Document
+                {
+                    Date = DateTime.Today,
+                    Title = TitleTxtbx.Text,
+                    FilePath = Request.PhysicalApplicationPath + "Uploads\\"
+
+                };
+                ScitMajorProjectDbContext dbContext = new ScitMajorProjectDbContext();
+                try
+                {
+                    dbContext.Documents.Add(uploadingDocument);
+                    dbContext.SaveChanges();
+                }
+                catch (DbEntityValidationException x)
+                {
+                    foreach (var eve in x.EntityValidationErrors)
+                    {
+                        Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:", eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        foreach (var ve in eve.ValidationErrors)
+                        {
+                            Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage);
+                        }
+                    }
+                    throw;
+                }
+
+
+            }
+        }
+    }
+}
