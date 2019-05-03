b41f33375526a5f988ddff08d27ce0d0e9623064
perplexedyawdie <sunkwenga@gmail.com>
2019-05-02 22:54:38 -0500

    Added gridView to display uploaded projects

---
+++ SMPP_web/SMPP_web/Account/Projects.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Projects.aspx.cs" Inherits="SMPP_web.Account.Projects" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="BannerContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="table-responsive" style="margin-top:5%;">
+        <asp:GridView ID="GridView1" runat="server" CssClass="table table-striped table-bordered table-hover" AllowPaging="true"></asp:GridView>
+    </div>
+</asp:Content>
---
+++ SMPP_web/SMPP_web/Account/Projects.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SMPP_web.Models;
+
+namespace SMPP_web.Account
+{
+    public partial class Projects : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Document projectFile = new Document();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Title", typeof(string));
+            dt.Columns.Add("Date", typeof(string));
+
+            foreach (string strfile in Directory.GetFiles(Server.MapPath("~/Uploads")))
+            {
+                FileInfo fi = new FileInfo(strfile);
+                ScitMajorProjectDbContext db = new ScitMajorProjectDbContext();
+                projectFile = (from proj in db.Documents
+                               where proj.Title == fi.Name.Substring(0, fi.Name.IndexOf("."))
+                               select proj).FirstOrDefault();
+                if (projectFile != null)
+                {
+                    dt.Rows.Add(fi.Name, projectFile.Date.ToString().Substring(0, projectFile.Date.ToString().IndexOf(" ")));
+                }
+                
+
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        
+    }
+}
